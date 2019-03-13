53790e849c64a7882a3a86e72107cbc1f73c6a60
Faraz <iamfarazbaig@gmail.com>
2019-03-13 15:57:05 +0000

    outputting ingredient dynamically

--- src/components/Food/Food.js
+++ src/components/Food/Food.js
@@ -3,13 +3,16 @@ import cssClasses from './Food.module.css'
 import FoodIngredient from './FoodIngredient/FoodIngredient'
 
 const food = (props) => {
+    const arrayIngredients = Object.keys(props.ingredients) //Object is plain js. keys method extracts keys of ingredient object and turns it into an array.value= how many ingredients,key=type of ingredient
+    .map(iKey => {
+         return [...Array(props.ingredients[iKey])].map((_, i) => {
+            return <FoodIngredient key = {iKey + i} type={iKey} />// return array of jsx.here type is iKey which is why inner map element type is given to be underscore
+         }) 
+    })//map executes a function on each element in the input array. inner map maps the elements.underscore is argument name to indicate its a blank but the index in needed
     return(
         <div className={cssClasses.Food}>
             <FoodIngredient type={"topBread"} />
-            <FoodIngredient type={"burgerPatty"} />
-            <FoodIngredient type={"cheese"} />
-            <FoodIngredient type={"chilli"} />
-            <FoodIngredient type={"lettuce"} />
+            {arrayIngredients}
             <FoodIngredient type={"bottomBread"} />
         </div>
     )
--- src/containers/FoodBuilder/FoodBuilder.js
+++ src/containers/FoodBuilder/FoodBuilder.js
@@ -1,14 +1,21 @@
 import React, {Component, Fragment} from 'react'
 import Food from '../../components/Food/Food'
 class FoodBuilder extends Component {
+    state= {
+        ingredients: { //key value pairs key:ingredients, value:amount
+            lettuce :1,
+            cheese: 1,
+            chilli:2,
+            burgerPatty:1
+        }
+    }
+
     render () {
         return (
             <Fragment>
-            <div>
-                 <Food />
-                 </div>{/*graphical rep of food with ingredients */}
-               <div>Build Controls</div>  {/*Add or remove ingredients */}
-               </Fragment>
+                <Food ingredients={this.state.ingredients}/>{/*Passing key value pairs of ingredients given. graphical rep of food with ingredients */}
+                <div>Build Controls</div>  {/*Add or remove ingredients */}
+            </Fragment>
         )
     }
 }
