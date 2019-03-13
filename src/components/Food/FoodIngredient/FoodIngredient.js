e56ab58a78ab983b34b99a9387f962c1d0fcdf31
Faraz <iamfarazbaig@gmail.com>
2019-03-13 07:33:26 +0000

    extended foodIngredient css and js files

--- src/components/Food/FoodIngredient/FoodIngredient.js
+++ src/components/Food/FoodIngredient/FoodIngredient.js
@@ -1,29 +1,43 @@
-import React from 'react'
+import React, {Component} from 'react'
+import PropTypes from 'prop-types'
 import cssClasses from './FoodIngredient.css'
-const foodIngredient = (props) => {
-    let ingredient = null
 
-    switch(props.type) {
-        case('topBread'):
-            ingredient=<div className={cssClasses.TopBread} />
-            break
-        case('burgerPatty'):
-            ingredient=<div className={cssClasses.Patty} />
-            break
-        case('cheese'):
-            ingredient=<div className={cssClasses.Cheese} />
-            break
-        case('chilli'):
-            ingredient=<div className={cssClasses.Chilli} />
-            break
-        case('lettuce'):
-            ingredient=<div className={cssClasses.Lettuce} />
-            break
-        case('bottomBread'):
-            ingredient=<div className={cssClasses.BottomBread} />
-            break
-        default:
-                ingredient=null 
-    }
-    return ingredient
+class FoodIngredient extends Component {
+    render () {
+        let ingredient = null
+
+        switch(this.props.type) {
+            case('topBread'):
+                ingredient=(
+                <div className={cssClasses.TopBread}>
+                <div className={cssClasses.Seeds1} />
+                <div className={cssClasses.Seeds2} />
+                </div>)
+                break
+            case('burgerPatty'):
+                ingredient=<div className={cssClasses.Patty} />
+                break
+            case('cheese'):
+                ingredient=<div className={cssClasses.Cheese} />
+                break
+            case('chilli'):
+                ingredient=<div className={cssClasses.Chilli} />
+                break
+            case('lettuce'):
+                ingredient=<div className={cssClasses.Lettuce} />
+                break
+            case('bottomBread'):
+                ingredient=<div className={cssClasses.BottomBread} />
+                break
+            default:
+                    ingredient=null 
+        }
+        return ingredient
+        }
 }
+
+FoodIngredient.propTypes = {
+    type:PropTypes.string.isRequired
+}
+
+export default FoodIngredient
